564b6a8cc35b72ff11e3c097c9e8234d0be420ed
k-srivastava <srivastavak1223@gmail.com>
2022-08-01 18:06:53 +0530

    Removed PEP8: E501 warning from docstring.

--- core/physics/vector.py
+++ core/physics/vector.py
@@ -121,7 +121,8 @@ class Vector2D:
         """
         Check whether the vector is normalized, i.e., its length is equal to one.
 
-        :param precision: Precision to which the length of the vector is to be checked to keep a delta for floating-point inaccuracies; defaults to 4.
+        :param precision: Precision to which the length of the vector is to be checked to keep a delta for
+        floating-point inaccuracies; defaults to 4.
         :type precision: int
 
         :return: True if the length pr magnitude of the vector is equal to one; else False.
