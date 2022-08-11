6996f3b083a88c78de38a596d123c7b306e8b8ca
k-srivastava <srivastavak1223@gmail.com>
2022-08-11 16:24:59 +0530

    Added documentation.

--- core/physics/world.py
+++ core/physics/world.py
@@ -1,3 +1,6 @@
+"""
+Game environment in Forge.
+"""
 # Constraints on the body sizes.
 MIN_BODY_AREA: float = 0.01 * 0.01
 MAX_BODY_AREA: float = 64 * 64
@@ -8,6 +11,16 @@ MAX_BODY_DENSITY: float = 25
 
 
 def verify_body_constraints(area: float, density: float) -> None:
+    """
+    Verify that a given body fits within Forge's world constraints of a certain area and density.
+
+    :param area: Area of the body.
+    :type area: float
+    :param density: Density of the body.
+    :type density: float
+
+    :raises ValueError: The body's area and density must lie within the given world constraints.
+    """
     if not MIN_BODY_AREA <= area <= MAX_BODY_AREA:
         raise ValueError(
             f'The area is not within the specified world limits. Area must be between {MIN_BODY_AREA} and '
