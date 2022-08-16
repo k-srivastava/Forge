1aeb920c2e6b49385dae0bdabdcdfce30d95f3b7
k-srivastava <srivastavak1223@gmail.com>
2022-08-16 15:33:24 +0530

    Added a settings.py file that can be changed dynamically at runtime as required.

---
+++ core/engine/settings.py
@@ -0,0 +1,8 @@
+"""
+Global settings for Forge.
+"""
+# Automatic addition to renderer settings.
+AUTO_ADD_IMAGES = False
+AUTO_ADD_STATIC_BODIES = False
+AUTO_ADD_RIGID_BODIES = False
+AUTO_ADD_LINES = False
