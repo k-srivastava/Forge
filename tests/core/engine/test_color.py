56f19e731179d28f46bae31ae4b7e9ff72ad204e
k-srivastava <srivastavak1223@gmail.com>
2022-08-16 15:34:36 +0530

    Added 8-bit RGBA colors and corresponding tests.

---
+++ tests/core/engine/test_color.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+import pygame.color
+
+from core.engine.color import Color
+
+
+class TestColor(TestCase):
+    def test_initialization(self):
+        self.assertRaises(ValueError, Color, 300, 200, 200)
+        self.assertRaises(ValueError, Color, 80, 90, 20, 20.4)
+
+    def test_as_tuple(self):
+        color = Color(120, 80, 20, 65)
+
+        self.assertEqual((120, 80, 20), color.as_tuple())
+        self.assertEqual((120, 80, 20, 65), color.as_tuple(with_alpha=True))
+
+    def test_as_pygame_color(self):
+        forge_color = Color(80, 97, 20)
+        pygame_color = pygame.color.Color(80, 97, 20)
+
+        self.assertEqual(pygame_color, forge_color.as_pygame_color())
